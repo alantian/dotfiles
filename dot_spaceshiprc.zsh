5b2d0d81420158257ea4f0631bfef517c77cfc94
Yingtao Tian <alan.yt.tian@gmail.com>
2023-03-10 17:04:40 +0900

    update spaceship config

---
+++ dot_spaceshiprc.zsh
@@ -0,0 +1,24 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_PROMPT_ORDER=(
+  user
+  dir
+  host
+  git
+  package
+  venv
+  conda
+  line_sep
+  exit_code
+  sudo
+  char
+)
+
+# Config options for prompt parts
