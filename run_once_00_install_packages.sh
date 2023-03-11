1a872679b9657c5476ca36055e0430ce4607b23b
Yingtao Tian <alan.yt.tian@gmail.com>
2023-03-11 10:22:18 +0900

    add new cli tools (for arch now)

--- run_once_00_install_packages.sh
+++ run_once_00_install_packages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# ref
+# References:
 #  - https://unix.stackexchange.com/questions/46081/identifying-the-system-package-manager
+#  - https://jvns.ca/blog/2022/04/12/a-list-of-new-ish--command-line-tools/
+
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -9,11 +11,38 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 if [ `uname` = "Linux" ]; then
-  if [ -f /etc/arch-release ] ; then
+  if [ -f /etc/arch-release ] ; then # Arch linux
     echo -e "${bold}Arch Linux detected${normal}"
-    echo -e "${bold}Install packages${normal}"
-    sudo pacman -S --noconfirm --needed \
-     vim zsh git 
+    echo -e "${bold}Run \`pacman -Syu\`${normal}"
+    sudo pacman -Syu
+    echo -e "${bold}Install yay for AUR${normal} if it's not installed"
+    if [ ! -f /usr/bin/yay ] ; then
+      (sudo pacman -Syu --noconfirm --needed git base-devel &&\
+      cd /tmp&&\
+      git clone https://aur.archlinux.org/yay.git &&\
+      cd yay && makepkg -si --noconfirm && cd /tmp && rm -rf yay ;)
+    fi
+    echo -e "${bold}Install packages using pacman${normal}"
+    sudo pacman -Syu --noconfirm --needed \
+      vim zsh git \
+      grep ripgrep `# grep` \
+      exa `# ls` \
+      bat `# cat` \
+      git-delta `# a pager for git` \
+      fd `# find` \
+      duf `# df` \
+      dust `# du` \
+      bottom btop glances gtop `# top` \
+      sd `# sed` \
+      difftastic `# df` \
+      plocate `# locate` \
+      hexyl `# hexdump` \
+    ;
+    echo -e "${bold}Install packages using yay${normal}"
+    yay -Syu --noconfirm --needed \
+      zenith `# top` \
+      nodejs-tldr `#man, sort of` \
+    ;
     echo -e "${bold}Change shell to zsh${normal}"
     sudo chsh -s /usr/bin/zsh $(whoami)
   fi
