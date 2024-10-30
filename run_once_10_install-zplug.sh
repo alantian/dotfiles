32e1d07bd33f27cd5befee49c3519da1e1dd9b8d
Yingtao Tian <alan.yt.tian@gmail.com>
2024-10-30 15:56:47 +0900

    prepare to use .chezmoiexternal.toml

--- run_once_10_install-zplug.sh
+++ run_once_10_install-zplug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ ! -d ~/.zplug ] ; then
-  git clone https://github.com/zplug/zplug ~/.zplug
-fi
+#if [ ! -d ~/.zplug ] ; then
+#  git clone https://github.com/zplug/zplug ~/.zplug
+#fi
