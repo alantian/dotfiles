d9a9d539509dc03a9014875dac278c0a28680b59
Yingtao Tian <alan.yt.tian@gmail.com>
2025-02-03 15:02:32 +0900

    Update .local/bin/oh-my-posh-update.sh

--- private_dot_local/bin/executable_oh-my-posh-update.sh
+++ private_dot_local/bin/executable_oh-my-posh-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ `uname` = "Linux" -o `uname` = "Darwin"]; then
+if [ `uname` = "Linux" -o `uname` = "Darwin" ]; then
   curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin -t ~/.poshthemes
 else
   warning "WARNING: cannot determine the OS."
