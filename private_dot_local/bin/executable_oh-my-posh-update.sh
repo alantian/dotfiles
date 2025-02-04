ab0e07240d45e00a72e606774f7876aff5a30748
Yingtao Tian <alan.yt.tian@gmail.com>
2025-02-04 22:41:27 +0900

    Update .zshrc Update .local/bin/oh-my-posh-update.sh

--- private_dot_local/bin/executable_oh-my-posh-update.sh
+++ private_dot_local/bin/executable_oh-my-posh-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ `uname` = "Linux" -o `uname` = "Darwin" ]; then
+echo "Install/upgrade oh-my-posh"
+
+if [ `uname` = "Linux" ] || [] `uname` = "Darwin" ] ; then
   curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin -t ~/.poshthemes
 else
   warning "WARNING: cannot determine the OS."
