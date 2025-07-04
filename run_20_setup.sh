8d3700da2274f142941e23776bf2d67687d9bc8e
Yingtao Tian <alan.yt.tian@gmail.com>
2025-07-04 17:31:56 +0900

    fix

--- run_20_setup.sh
+++ run_20_setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(curl -fsSL https://raw.githubusercontent.com/alantian/setup-scripts/main/all.sh | bash )
+( curl -fsSL https://raw.githubusercontent.com/alantian/setup-scripts/main/all.sh | bash )
